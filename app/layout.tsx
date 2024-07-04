57baad2e47c6b174e17b0fd452211f3b7def0ab0
Vijay Kumar Murakada <cod36324@adobe.com>
2024-07-04 16:54:02 +0530

    navbar-pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
-export default function RootLayout({
-  children,
-}: {
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata ={
+title: 'Next.js-project'
+}
+
+export default function RootLayout({ children,}: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
